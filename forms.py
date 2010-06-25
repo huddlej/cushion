a60d2acfb26ae2f197867c248ffd0f3197795f61
John Huddleston <huddlej@gmail.com>
2010-06-24 21:08:54 -0700

    added start to function to get a form for a document.

--- forms.py
+++ forms.py
@@ -9,3 +9,45 @@ class DocumentForm(forms.Form):
 
 class UploadFileForm(forms.Form):
     file = forms.FileField(label="Attach a file:")
+
+
+def get_form_for_document(document):
+    """
+    Returns a Django form with fields based on the data types of the given
+    CouchDB document.
+    """
+    # Get the class name.
+    class_name = document.get("type", document["_id"])
+    log.debug("class name: %s" % class_name)
+
+    # Type to form field mappings.
+    type_to_field = {
+        int: forms.IntegerField,
+        float: forms.FloatField,
+        str: forms.CharField,
+        unicode: forms.CharField,
+        bool: forms.BooleanField
+    }
+
+    # Convert field types into Field objects.
+    fields = {}
+
+    for field_name, value in document.items():
+        field_type = type_to_field.get(type(value))
+        if field_type is not None:
+
+
+        field = getattr(forms, field_type)(**field_definition)
+        fields[field_name] = field
+    log.debug("fields: %s" % fields)
+
+    # Create the form from the class name and Field objects.
+    form = type(class_name, (forms.Form,), fields)
+
+    # If there is a description set it as the doc string.
+    if description:
+        form.__doc__ = description
+
+    log.debug("form: %s" % form)
+    return form
+
