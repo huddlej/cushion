07c023641ac6d1f75f1fbf185b01a10a07c7552f
John Huddleston <huddlej@gmail.com>
2010-07-12 18:11:13 -0700

    reworked _all_docs url pattern to simplify reverse lookup from the templates.

--- urls.py
+++ urls.py
@@ -5,7 +5,7 @@ from views import index, database, view, document
 
 urlpatterns = patterns("",
     url(r"^$", index, name="cushion_index"),
-    url(r"^(?P<database_name>\w+)/_all_docs/$", view, {"view_name": "_all_docs"}, name="cushion_view"),
+    url(r"^(?P<database_name>\w+)/(?P<view_name>_all_docs)/$", view, name="cushion_view"),
     url(r"^(?P<database_name>\w+)/_design/(?P<design_doc_name>\w+)/_view/(?P<view_name>\w+)$", view, name="cushion_view"),
     url(r"^(?P<database_name>\w+)/(?P<document_id>\w+)/$", document, name="cushion_document"),
     url(r"^(?P<database_name>.+)/$", database, name="cushion_database"),
