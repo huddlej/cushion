4ae07c99a49788d2505e2bef7f8080e290798913
John Huddleston <huddlej@gmail.com>
2010-07-13 16:32:27 -0700

    reordered url patterns so most specific patterns occur first.

--- urls.py
+++ urls.py
@@ -5,8 +5,8 @@ from views import index, database, view, document
 
 urlpatterns = patterns("",
     url(r"^$", index, name="cushion_index"),
+    url(r"^(?P<database_name>\w+)/_design/(?P<design_doc_name>\w+)/_view/(?P<view_name>.+)/$", view, name="cushion_view"),
     url(r"^(?P<database_name>\w+)/(?P<view_name>_all_docs)/$", view, name="cushion_view"),
-    url(r"^(?P<database_name>\w+)/_design/(?P<design_doc_name>\w+)/_view/(?P<view_name>\w+)$", view, name="cushion_view"),
     url(r"^(?P<database_name>\w+)/(?P<document_id>\w+)/$", document, name="cushion_document"),
     url(r"^(?P<database_name>.+)/$", database, name="cushion_database"),
 #     url(r"^(?P<doc_id>.+)/edit/$", edit, name="cushion_edit"),
