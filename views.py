58b7f079a86bb93827093c4e5d245a00af6d5aae
John Huddleston <huddlej@gmail.com>
2010-07-14 14:58:42 -0700

    reorganized imports.

--- views.py
+++ views.py
@@ -9,7 +9,11 @@ from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
-from forms import DocumentForm, UploadFileForm, get_form_for_document
+from forms import (
+    DocumentForm,
+    UploadFileForm,
+    get_form_for_document
+)
 
 
 def index(request):
