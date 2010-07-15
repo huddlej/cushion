f3d4832771e4830bfd6ee1df7c1d13fa53ecac2b
John Huddleston <huddlej@gmail.com>
2010-07-15 14:42:46 -0700

    added doctests for example data model.

--- tests.py
+++ tests.py
@@ -5,19 +5,19 @@ unittest). These will both pass when you run "manage.py test".
 Replace these with more appropriate tests for your application.
 """
 
-from django.test import TestCase
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
-
 __test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
-
->>> 1 + 1 == 2
-True
+>>> from cushion.models import Specimen
+>>> d = {"genus": "Autographa", "species": "ampla", "latitude": "45.0", "longitude": 112.0}
+>>> s = Specimen(**d)
+>>> s["latitude"]
+45.0
+>>> d = {"genus": "Autographa", "species": "ampla", "latitude": "abc", "longitude": 112.0}
+>>> try:
+...     s = Specimen(**d)
+... except ValueError:
+...     print "ValueError"
+...     
+ValueError
+>>>
 """}
 
