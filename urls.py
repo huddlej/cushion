241e7aa8b9fae6ce4fc7fc585c278f15cefab152
John Huddleston <huddlej@gmail.com>
2010-08-09 20:08:24 -0700

    added support for slug in all docs view url.

--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from views import index, database, view, document
 urlpatterns = patterns("",
     url(r"^$", index, name="cushion_index"),
     url(r"^(?P<database_name>\w+)/_design/(?P<design_doc_name>\w+)/_view/(?P<view_name>.+)/$", view, name="cushion_view"),
-    url(r"^(?P<database_name>\w+)/(?P<view_name>_all_docs)/$", view, name="cushion_view"),
+    url(r"^(?P<database_name>[-\w]+)/(?P<view_name>_all_docs)/$", view, name="cushion_view"),
     url(r"^(?P<database_name>.+)/(?P<document_id>.+)/$", document, name="cushion_document"),
     url(r"^(?P<database_name>.+)/$", database, name="cushion_database"),
 #     url(r"^(?P<doc_id>.+)/edit/$", edit, name="cushion_edit"),
